db8b8e0b3d9bb296aae78f981c7970b600260e33
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-08-28 09:01:08 +0530

    day 2(2)

---
+++ Datatypes_Practices/Datatypes.java
@@ -0,0 +1,25 @@
+class Datatypes
+{
+	public static void main(String args[])
+	{
+		byte guru=6;
+		System.out.println("Value of byte is "+guru);
+		System.out.println("******"+guru+"******");
+		System.out.println(guru+" is stored in variable");
+		
+		
+		short b=1000;
+		System.out.println("b="+b);
+		
+		
+		int c=14421424;
+		System.out.println("Int Value is "+c);
+		
+		long d=1421412412;
+		System.out.print("The value of Long is "+d+ "It can be used to store mobile number or population");
+	
+		
+	
+	
+	}
+}
---
+++ Datatypes_Practices/Datatypes_2.java
@@ -0,0 +1,8 @@
+class Datatypes_2
+{
+	public static void main(String args[])
+	{
+		float a=12.96f;
+		System.out.print("float value is "+a+" f needs to be written at the last of value, if not then system will think the value is double ");
+	}
+}
