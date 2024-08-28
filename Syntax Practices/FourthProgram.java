fcaa887e0b0d2afdfbfbb888f9f837510702bd55
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-08-28 08:03:46 +0530

    day 2(1)

---
+++ Syntax Practices/FourthProgram.java
@@ -0,0 +1,9 @@
+class FourthProgram
+{
+		public static void main(String args[])
+		{
+				System.out.println("*************************");
+				System.out.println("       Gurjeet");
+				System.out.print("*************************");
+		}	
+}
---
+++ Syntax Practices/ThirdProgram.java
@@ -0,0 +1,12 @@
+class ThirdProgram{
+	
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.print("World");
+		
+		
+	}
+	
+}
