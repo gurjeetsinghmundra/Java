2217b51dc0a583f46f7f64d4c3f33a6157d74239
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-08-30 08:10:28 +0530

    day4(1)

---
+++ Java Basic Practices/logical_operators.java
@@ -0,0 +1,16 @@
+class logical_operators
+{
+		public static void main(String args[])
+		{
+			int a=3;
+			
+			System.out.println(a!=0 && a==3);
+		
+			System.out.println(!(a%2==0 || a%3==0));
+			
+			
+			System.out.println(!true);
+		
+		}
+	
+}
---
+++ Java Basic Practices/statement.java
@@ -0,0 +1,17 @@
+class statement
+{
+//Relational Operators	
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=12;
+		int c=10;
+		
+		
+		System.out.println(a>b);
+		System.out.println(a>=c);
+		System.out.println(a!=b);
+		
+	}
+	
+}
