7e192a8883d14d18db14f3529b780868283ed209
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-02 08:02:17 +0530

    day 5(1)

---
+++ 3_Java Operator Practices/string.java
@@ -0,0 +1,10 @@
+class string
+{
+	public static void main(String args[])
+	{
+			
+		String name="Gurjeet";
+		System.out.println("My Name is : "+name);
+
+	}
+}
---
+++ 4_Java Practices/Program1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Program1
+{
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+					
+					
+			Scanner sd=new Scanner(System.in);
+			int b=sd.nextInt();	
+			
+			System.out.println("Total Value: "+(a+b));		
+		}
+}
