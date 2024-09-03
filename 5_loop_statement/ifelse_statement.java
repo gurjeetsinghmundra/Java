b512ea9123b009377cc6c7ca1c5df433880572a6
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-03 08:18:15 +0530

    day 6(2)

---
+++ 5_loop_statement/ifelse_statement.java
@@ -0,0 +1,15 @@
+class ifelse_statement
+{
+		public static void main(String args[])
+		{
+			if(false)
+			{
+				System.out.println("If block");
+			}
+			else
+			{	
+				System.out.println("Else block");
+            }   	
+	
+		}
+}
---
+++ 5_loop_statement/subscribe_project.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class subscribe_project
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean subscribed=false;
+		
+		System.out.print("Do You Want To Subscribe?? (y/n)");
+		char decision=sc.next().charAt(0);
+		
+		if(decision=='y' || decision=='Y')
+		{
+				subscribed=true;
+		}
+		
+		System.out.println("Subscribed:"+subscribed);
+	}
+}
