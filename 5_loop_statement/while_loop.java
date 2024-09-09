0ee8c82bea7691328fd5ffce08e2efca130bedfa
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-09 07:51:47 +0530

    Day 10(1)

---
+++ 5_loop_statement/while_loop.java
@@ -0,0 +1,18 @@
+class while_loop
+{
+		public static void main(String args[])
+		{
+			//While loop consist of 3 Steps
+			
+			//1) Initialization
+			int i=1;
+			while(i<=30)
+			{
+				//2) Condition
+				System.out.println("Hello");
+				
+				//3) Increment or Decrement
+				i++;
+			}
+		}	
+}
