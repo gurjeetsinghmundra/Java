fc421d0201d43cfd310da16c896292e4f7381491
sanchit930 <jsanchit930@gmail.com>
2024-09-12 08:02:58 +0530

    Day 13(1)

---
+++ 6_logic_building/primenumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class primenumber
+{
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			int count=0;
+			
+			for(int i=1;i<=n;i++)
+			{	
+				if(n%i==0)
+				{
+					System.out.println(i+" ");
+					count++;
+				}
+			}
+			
+			System.out.println("Count: "+count);
+			
+			if(count==2)
+			{
+				System.out.println("Number is Prime");
+			}
+			else
+			{
+				System.out.println("Number is Not Prime");
+			}
+		}
+}
