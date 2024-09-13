1df6d5b44009006e7ca3d9bcbaffdf2e61e0dc2d
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-13 09:00:47 +0530

    Day 14(2)

---
+++ 6_logic_building/continue_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class continue_1
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+
+		for(int i=1;i<=5;i++)
+		{
+			if(i==3)
+				System.out.println(i);
+				continue;
+		}
+		
+		int sum=0;
+		for(int i=1;i<=5;i++)
+		{
+			int n=sc.nextInt();
+			if (n<0)
+				continue;
+			sum=sum+n;
+		}
+		System.out.println("Sum is "+sum);
+	}
+}
