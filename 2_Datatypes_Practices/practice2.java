dbeb6a26348f4e60b49e45872396a111c385db9a
Gurjeet Singh <161136281+gurjeetsinghmundra@users.noreply.github.com>
2024-09-15 01:07:14 +0530

    Day 16(1)

--- 2_Datatypes_Practices/practice2.java
+++ 2_Datatypes_Practices/practice2.java
@@ -4,6 +4,7 @@ class practice2
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
     System.out.println();
     System.out.println("Hello World");
   }
