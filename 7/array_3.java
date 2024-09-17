c69ea8af37218389cf6f581fcfb6d5a5cb19f353
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-17 08:07:22 +0530

    Day 18(1)

---
+++ 7/array_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class array_3
+{
+	// Comand Line arguments
+	public static void main(String args[])
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		
+	}
+}
---
+++ 7/array_4.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+class array_4
+{
+	
+	public static void main(String args[])
+	{
+
+		Scanner sc=new Scanner(System.in);
+		String weekdays[]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+					  //    0      1	 2     3     4     5     6  
+		int primeNumbers[]={11,13,17,5};
+		
+		System.out.println("Weekdays:"+weekdays.length);
+		System.out.println("PrimeNumbers:"+primeNumbers.length);
+
+		//Enhanced For Loop
+		
+			for(int number:primeNumbers)
+			{
+					System.out.println(number);
+		
+			}
+			
+		int numbers[]={1,4,6,2,45};
+		int sum=0;
+
+			for(int num:numbers)
+			{
+				sum=num+sum;
+				
+			}
+			System.out.println(sum);
+			
+//-------------------------------------------------------------------
+
+			char ch[]={'a','e','i','a','c','a'};
+			int count=0;
+			
+			for(char c:ch)
+			{
+				if(c=='a')
+					count++;	
+			}
+			
+			System.out.println(count);
+			
+System.out.println("=====================================================");
+
+
+			char chr[]={'a','e','t','i'};
+			
+			for(char cc:chr)
+			{
+				if(cc=='a' || cc=='e' || cc=='i')
+					System.out.println("Vowels");
+				else
+					System.out.println("Consonants");	
+			}
+		
+	}
+}
