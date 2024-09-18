f2bb487896118d51557dc32372ccc84ae84bda35
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-18 07:55:18 +0530

    Day 18(1)

---
+++ 7/array_2dimensional.java
@@ -0,0 +1,11 @@
+class Array_multid
+{
+	public static void main(String args[])
+	{
+		//One dimensional         Multi dimensional(2D Two Dimensional)
+		int numbers[][]={{46,12,76},{13,35,54}};
+	
+		System.out.println(numbers[0][1]);
+	
+	}
+}
---
+++ 7/array_6.java
@@ -0,0 +1,16 @@
+class array_6
+{
+	public static void main(String args[])
+	{
+		int arr1[][]={{22,53,34},{56,43,13}};
+	
+		for (int i=0;i<arr1.length;i++)
+		{
+			for(int j=0;j<arr1[0].length;j++)
+			{
+				System.out.println(arr1[i][j]);
+			}
+		}
+
+	}
+}
