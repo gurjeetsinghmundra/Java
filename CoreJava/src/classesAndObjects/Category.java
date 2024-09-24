67597db100259391ca708a2009b74475dd4cde9c
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-24 07:42:43 +0530

    Day 25(1)

--- CoreJava/src/classesAndObjects/Category.java
+++ CoreJava/src/classesAndObjects/Category.java
@@ -2,7 +2,15 @@ package classesAndObjects;
 
 public class Category {
 	
+	//JAVA Consist of default constructor which is used 
 	int categoryId;
 	String categoryName;
+	
+	//Non-parameterized Constructor
+	//Constructor name always same as class name
+	Category()
+	{
+		System.out.println("Category object is created using constructor");
+	}
 
 }
--- CoreJava/src/classesAndObjects/Example.java
+++ CoreJava/src/classesAndObjects/Example.java
@@ -3,7 +3,7 @@ package classesAndObjects;
 public class Example {
 	public static void main(String[] args) {
 		
-		Product p1=new Product();
+	/*	Product p1=new Product();
 		
 		p1.setProductDetails(101,"Computer",70000);
 		
@@ -11,6 +11,10 @@ public class Example {
 		p2.setProductDetails(102,"Mobile",20000);
 		
 		System.out.println("You have to Pay: "+p2.discount());
+		
+	*/
+		new Category();
+		new Category();
 	}
 
 }
