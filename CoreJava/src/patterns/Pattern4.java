f16802165ec3e83530ef58953bcd860e0e8e6ff7
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-09-28 09:12:49 +0530

    Day 29(2)

--- CoreJava/src/patterns/Pattern4.java
+++ CoreJava/src/patterns/Pattern4.java
@@ -36,7 +36,101 @@ System.out.println("====================================================");
 			}
 			System.out.println();
 		}
+			
+System.out.println("====================================================");	
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)//main logic j<=i (i ki value jitni utne * print honge)
+			{
+				if(i%2==0)
+				{
+					System.out.print(i);
+				}
+				else
+				{
+					System.out.print("*");
+				}
 				
+			}
+			System.out.println();
+		}
+			
+System.out.println("====================================================");	
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)//main logic j<=i (i ki value jitni utne * print honge)
+			{
+				if(i%2==0)
+				{
+					System.out.print(j);
+				}
+				else
+				{
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println();
+		}	
+
+System.out.println("====================================================");	
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)//main logic j<=i (i ki value jitni utne * print honge)
+			{
+				if(i%2==0)
+				{
+					System.out.print("0");
+				}
+				else
+				{
+					System.out.print("1");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	System.out.println("====================================================");	
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)//main logic j<=i (i ki value jitni utne * print honge)
+			{
+				if((i+j)%2==0)
+				{
+					System.out.print("1");
+				}
+				else {
+					System.out.print("0");
+				}
+					
+				
+				
+			}
+			System.out.println();
+		}		
+		
+System.out.println("====================================================");	
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==1||j==1||i==5||j==5||i==j)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}			
 		
 	}
 }
