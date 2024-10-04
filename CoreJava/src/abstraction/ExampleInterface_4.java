719e348e68bde511adc19d5832e0f8402ebcc825
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-10-04 08:58:14 +0530

    Day 33(2)

--- CoreJava/src/abstraction/ExampleInterface_4.java
+++ CoreJava/src/abstraction/ExampleInterface_4.java
@@ -7,6 +7,21 @@ interface I6
 	void printName(String name);
 }
 
+@FunctionalInterface
+
+interface I7
+{
+	int add(int a,int b);
+}
+
+@FunctionalInterface
+
+interface I8
+{
+	int square(int n);
+}
+
+
 public class ExampleInterface_4 {
 	
 	public static void main(String[] args) {
@@ -23,6 +38,24 @@ public class ExampleInterface_4 {
 		};
 		
 		obj1.printName("Gurjeet");
+		
+		I7 obj2=(a,b)-> a+b; //it is not required to write RETURN in lambda
+		//normally we write return a+b 
+		
+		//1 way
+		int ans=obj2.add(5, 10);
+		System.out.println(ans);
+		
+		//2 way
+		System.out.println(obj2.add(12,10));
+		//void type cant be written in sysout coz it doesnt written anything
+		
+		
+System.out.println("======================================================================");
+
+		I8 s=(n)-> n*n;
+		System.out.println(s.square(10));
+		
 		
 	}
 
