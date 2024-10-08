236ac1ef13ccd18a06cfcbf2db62a2e5bcf8be25
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-10-08 08:05:04 +0530

    Day 37(1)

--- CoreJava/src/stringExamples/Example4.java
+++ CoreJava/src/stringExamples/Example4.java
@@ -8,5 +8,40 @@ public class Example4 {
 		s1=s1.concat("World");
 		
 		System.out.println(s1);
+		
+		System.out.println("-----------------------------------------------------");
+		
+		
+		System.out.println("   He         ll          oo     ".trim()+"World");
+		//.trim() will remove space from start and end
+		
+		
+		System.out.println("Gurjeet".length());
+		System.out.println("Guru".charAt(2)); //index starts at zero
+		
+		System.out.println("-----------------------------------------------------");
+		
+		System.out.println("Apple".indexOf('p'));
+		//Will give index of first p
+		
+		System.out.println("Laptop".indexOf('p',3));//after comma represents index
+		//will give index of p after 3rd index
+		
+		System.out.println("-----------------------------------------------------");
+		
+		System.out.println("Laptop".startsWith("La")); //Case Sensitive
+		
+		System.out.println("-----------------------------------------------------");
+		
+		String s2="Bottle";
+		char arr[]=s2.toCharArray();
+		
+		for(char ch:arr)
+		{
+			System.out.println(ch);
+		}
+		
+		
+		
 	}
 }
