9c2834eefdb661eb6a156529b44b38c167a2e6d2
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-10-21 08:58:01 +0530

    Day 50(2)

--- CoreJava/src/learnCollection/ListExample10.java
+++ CoreJava/src/learnCollection/ListExample10.java
@@ -1,5 +1,6 @@
 package learnCollection;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class ListExample10 {
@@ -11,6 +12,28 @@ public class ListExample10 {
 		products.push(new Product(101,"Laptop",50000));
 		products.push(new Product(102,"Mobile",35000));
 		products.push(new Product(103,"Headphones",2000));
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Do You Want To Remove Last Added Product?(Y/N)");
+		System.out.println(products.peek());
+		
+		char decision=sc.next().charAt(0);
+		
+		if(decision=='Y' || decision=='y')
+		{
+			
+		System.out.println(products.pop().getName()+" Is Removed");
+		
+		System.out.print("Items In Cart "+products); //solve
+		
+		}
+		else
+		{
+			System.out.println("Not Removed");
+		}
+				
+		
 		
 	}
 
---
+++ CoreJava/src/learnCollection_2/SetExample1.java
@@ -0,0 +1,37 @@
+package learnCollection_2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class SetExample1 {
+	
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set=new HashSet<Integer>();
+		
+		set.add(12);
+		set.add(67);
+		set.add(45);
+		set.add(67);
+		set.add(null);
+		
+	//will not take duplicates value and will insert element randomly
+	//
+	//hashset will not maintain insertion order,and will take unique values and also null values is allowed
+		
+		
+	System.out.println(set);
+	
+	System.out.println("===============================================");
+	
+	List<Integer> list= Arrays.asList(12,67,67,89,90,90);
+	System.out.println(list);
+	
+	HashSet<Integer> hashSet=new HashSet<Integer>(list);
+	
+	System.out.println(hashSet);
+		
+	}
+
+}
