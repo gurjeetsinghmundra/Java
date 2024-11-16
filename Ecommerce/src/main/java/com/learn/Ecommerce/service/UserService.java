948f51b5c9e6dd237bb790add4def6bb25009d12
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-11-16 07:44:18 +0530

    Day 75(2)

---
+++ Ecommerce/src/main/java/com/learn/Ecommerce/service/UserService.java
@@ -0,0 +1,26 @@
+package com.learn.Ecommerce.service;
+
+import java.util.List;
+
+import com.learn.Ecommerce.Dto.UserDto;
+
+public interface UserService {
+	
+	//To add User
+	UserDto addUser(UserDto userDto);
+	
+	//To Fetch all users
+	List<UserDto> getAllUsers();
+	
+	//To Fetch user by Id
+	UserDto	getUserById(String id);
+	
+	//Update User By Id
+	UserDto updateUser(String id,UserDto userDto);
+	
+	//Delete User By Id
+	String deleteUser(String id);
+	//String returntype is used because the method will return  void
+	//and instead of void we can display mssg "id deleted successfully"
+	
+}
