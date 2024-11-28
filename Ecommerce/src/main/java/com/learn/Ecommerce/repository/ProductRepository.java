656935d27989e7dddee3cb93105cf6f31ea8d4b0
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-11-28 09:04:38 +0530

    Day 84(2)

--- Ecommerce/src/main/java/com/learn/Ecommerce/repository/ProductRepository.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/repository/ProductRepository.java
@@ -1,10 +1,33 @@
 package com.learn.Ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.Product;
 
 //Repository is for database									  //id
 public interface ProductRepository extends JpaRepository<Product, Integer>{
-
+	
+	//JPA Query Methods
+	
+	// products/search in postman to get all links
+	// /search/findByProductPriceLessThanEqual?price=50000
+	
+	//mutiple products matlab list of products
+	
+	List<Product> findByProductPriceLessThan(int price);
+	
+	List<Product> findByProductPriceGreaterThan(int price);
+	
+	List<Product> findByProductPriceGreaterThanEqual(int price);
+	
+	List<Product> findByProductPriceLessThanEqual(int price);
+	
+	// search/findByProductPriceBetween?startPrice=40000&endPrice=80000
+	
+	List<Product> findByProductPriceBetween(int startPrice,int endPrice); //between ke liye 2
+	
 }
+
+				
--- Ecommerce/src/main/java/com/learn/Ecommerce/repository/UserRepository.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.learn.Ecommerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.User;
@@ -8,5 +11,12 @@ import com.learn.Ecommerce.entity.User;
 public interface UserRepository extends JpaRepository<User, String> {
 	
 	
-
+	Optional<User> findByEmailId(String email);
+	
+	List<User> findByFirstName(String firstName);
+	
+	List<User> findByLastName(String lastName);
+	
+	List<User> findByFirstNameStartingWith(String Name);
+	
 }
