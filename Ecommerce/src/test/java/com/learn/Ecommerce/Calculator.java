cc55ec16788ee15999ce0c95d1197e4938d4843d
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2025-02-25 12:19:23 +0530

    JUnit Testing

---
+++ Ecommerce/src/test/java/com/learn/Ecommerce/Calculator.java
@@ -0,0 +1,26 @@
+package com.learn.Ecommerce;
+
+public class Calculator {
+
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	
+	
+	public boolean isEven(int n)
+	{
+		return n%2==0;
+	}
+	
+}
--- Ecommerce/src/test/java/com/learn/Ecommerce/EcommerceApplicationTests.java
+++ Ecommerce/src/test/java/com/learn/Ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,56 @@
 package com.learn.Ecommerce;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceApplicationTests {
+	
+	@Autowired
+	CustomerService customerService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testAdd() {
+		Calculator calculator = new Calculator();
+		int actualResult = calculator.add(10,2);
+		int expectedResult=12;
+		assertEquals(expectedResult,actualResult);
+	}
+	
+	@Test
+	public void testSub() {
+		Calculator calculator = new Calculator();
+		int actualResult = calculator.sub(10,2);
+		int expectedResult=8;
+		assertEquals(expectedResult,actualResult);
+	}
+	
+	@Test
+	public void testMul() {
+		Calculator calculator = new Calculator();
+		int actualResult = calculator.mul(10,2);
+		int expectedResult=20;
+		assertEquals(expectedResult,actualResult);
+	}
+	
+	@Test
+	public void testIsEven() {
+		Calculator calculator = new Calculator();
+		int n=7;
+		assertEquals(false,calculator.isEven(n));
+	}
+	
+	@Test
+	public void testGetCustomer() 
+	{
+		assertNotNull(customerService.getCustomer(101, "Gurjeet"));
+	}
+	
 }
